48804b412a714f82fe1a1f6c6cc11c272cc549de
ayuka <basketball110512@gmail.com>
2024-04-30 08:08:29 +0900

    contact.blade.php修正

--- src/database/seeders/CategoriesTableSeeder.php
+++ src/database/seeders/CategoriesTableSeeder.php
@@ -14,18 +14,39 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        $contents = [
-            "商品のお届けについて",
-            "商品の交換について",
-            "商品トラブル",
-            "ショップへのお問い合わせ",
-            "その他"
+        $param = [
+            $content => '商品のお届けについて',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
         ];
+        DB::table('categories')->insert($param);
 
-        foreach( $contents as $content ) {
-            DB::table('categories')->insert([
-                'content' => $content,
-            ]);
-        }
+        $param = [
+            $content => '商品の交換について',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ];
+        DB::table('categories')->insert($param);
+
+        $param = [
+            $content => '商品トラブル',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ];
+        DB::table('categories')->insert($param);
+ 
+        $param = [
+            $content => 'ショップへのお問い合わせ',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ];
+        DB::table('categories')->insert($param);
+        
+        $param = [
+            $content => 'その他',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ];
+        DB::table('categories')->insert($param);
     }
 }
--- src/resources/views/contact.blade.php
+++ src/resources/views/contact.blade.php
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>ブラウザに画面を表示できた！</h1>
-</body>
-</html>
-=======
 @extends('layouts.app')
 
 @section('css')
@@ -20,4 +7,4 @@
 @section('content')
 
 @endsection
->>>>>>> 1f4302e (開発環境作成)
+
