4910788e537cbc77eb6499b561c42abdfd7bcbe4
ayuka <basketball110512@gmail.com>
2024-05-01 08:42:28 +0900

    バリデーションの設定

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Models\Contact;
+use App\Http\Requests\ContactRequest;
 use Illuminate\Support\Facades\Date;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 
@@ -14,5 +15,12 @@ class ContactController extends Controller
     {
         $categories = Category::all();
         return view('contact', compact('categories'));
+    }
+
+    public function confirm(ContactRequest $request)
+    {
+        $contacts = $request->all();
+        $category = Category::find($request->category_id);
+        return view('confirm', compact('contacts', 'category'));
     }
 }
