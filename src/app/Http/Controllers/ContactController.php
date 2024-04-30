b13adacbdf9fb4d4973abc8783f235657e97a852
ayuka <basketball110512@gmail.com>
2024-04-30 20:49:23 +0900

    contact画面の表示まで確認

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Contact;
+use Illuminate\Support\Facades\Date;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class ContactController extends Controller
 {
     public function index()
     {
-        return view('contact');
+        $categories = Category::all();
+        return view('contact', compact('categories'));
     }
 }
