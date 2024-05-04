315a7d72f77734714944e58ed24ecafb63344866
ayuka <basketball110512@gmail.com>
2024-05-05 07:18:51 +0900

    Fortify設定

--- src/app/Providers/FortifyServiceProvider.php
+++ src/app/Providers/FortifyServiceProvider.php
@@ -3,15 +3,14 @@
 namespace App\Providers;
 
 use App\Actions\Fortify\CreateNewUser;
-use App\Actions\Fortify\ResetUserPassword;
-use App\Actions\Fortify\UpdateUserPassword;
-use App\Actions\Fortify\UpdateUserProfileInformation;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 use Laravel\Fortify\Fortify;
+use Laravel\Fortify\Http\Requests\LoginRequest as FortifyLoginRequest;
+use App\Http\Requests\LoginRequest;
 
 class FortifyServiceProvider extends ServiceProvider
 {
@@ -28,19 +27,27 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // 新規ユーザーの登録
         Fortify::createUsersUsing(CreateNewUser::class);
-        Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
-        Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
-        Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
 
+        // 登録画面を表示したときに表示する内容の設定
+        Fortify::registerView(function(){
+            return view('auth.register');
+        });
+
+        // ログイン画面を表示したときに表示する内容の設定
+        Fortify::loginView(function(){
+            return view('auth.login');
+        });
+
+        // ログイン試行回数の制限の設定
         RateLimiter::for('login', function (Request $request) {
-            $throttleKey = Str::transliterate(Str::lower($request->input(Fortify::username())).'|'.$request->ip());
+            $email = (string) $request->email;
 
-            return Limit::perMinute(5)->by($throttleKey);
+            return Limit::perMinute(5)->by($email . $request->ip());
         });
 
-        RateLimiter::for('two-factor', function (Request $request) {
-            return Limit::perMinute(5)->by($request->session()->get('login.id'));
-        });
+        // ログインとバリデーションの結び付け
+        $this->app->bind(FortifyLoginRequest::class, LoginRequest::class);
     }
 }
